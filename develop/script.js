14db40a6b45c84239f471a1736799bfb1bcfba9f
Indigo <indigo.crandell@gmail.com>
2022-08-07 15:06:02 -0400

    OH YAY. Math.floor helped me on this. The box in the current hour is RED.

--- develop/script.js
+++ develop/script.js
@@ -4,25 +4,30 @@ var taskBox = document.querySelectorAll("task");
 // var clickMe = document.querySelectorAll("click-me");
 var calendar = 'file:///Users/indigofobes/Desktop/GT_Coding/plan-your-day/develop/index.html';
 var form = 'form.html';
+var now = moment().format("HH.mm");
+var time = document.getElementsByClassName("time");
 
 
 // Display current date at the top of the page
 var today = moment().format("dddd, MMMM Do");
 currentDay.textContent = today;
 
-// Change the color if event is past or present
-var now = moment().format("HHmm");
-console.log(now);
-var time = document.getElementsByClassName("time");
 
+
+// Change the color if event is past or present
+// Loop to access all elements with 'time' class.
 for (var i = 0; i < time.length; i++) {
   console.log(time[i].id);
+  // If the time on the planner is greater than the current time, make it green (future event).
   if (time[i].id >= now) {
+    time[i].setAttribute("style", "background-color: rgb(118, 159, 118)");
+  }
+  // If the 
+  var roundedDown = Math.floor(now);
+  if (time[i].id == roundedDown) {
     time[i].setAttribute("style", "background-color: #ff6961");
   }
-  //var timeBlock = time[i].innerHTML;
-  // console.log(timeBlock);
-  // If time block is greater than now, make the block GREEN
+  
 }
 
 
