8076e8bd21f5f1573c46357037377d6f972f23d8
Indigo <indigo.crandell@gmail.com>
2022-08-08 20:10:48 -0400

    Not much changed. I have been trying to do a couple of things. First, I want to change the background color of the task section, while still showing the input and typed words. Second, I want to make my save button clickable. From there, I want to get the user input value and store it locally so that the user can refresh the page and still have those tasks available.

--- develop/script.js
+++ develop/script.js
@@ -1,7 +1,6 @@
 // variables
 var currentDay = document.getElementById("currentDay");
 // var tableRows = document.getElementsByTagName("td");
-var taskBox = document.getElementsByClassName("task");
 // var clickMe = document.querySelectorAll("click-me");
 var calendar = 'file:///Users/indigofobes/Desktop/GT_Coding/plan-your-day/develop/index.html';
 var form = 'form.html';
@@ -30,49 +29,21 @@ for (var i = 0; i < time.length; i++) {
   }
 }
 
-// On click... add input field to empty div
-document.addEventListener('DOMContentLoaded',domloaded,false);
-function domloaded(){
+// function to handle input entries
+function handleInput(event) {
 
-  // loop to access each empty div
- /* for (var w = 0; w < displayInput.length; w++) {
-    console.log(displayInput[w]);
-  } */
+  // Grab user's task
+ // $('input').each(function (index) {
+    var userTask = $('input[name="todo"]').val();
+    console.log("clicked");
+  
+}
 
-  for (var i = 0; i < taskBox.length; i++) {
-    taskBox[i].addEventListener("click", function() {
-      console.log(taskBox[i]);
-      var newDiv = document.createElement("div");
-      taskBox[i].appendChild(newDiv);
-    });
-};
-}; 
+// Want it so that when user clicks save, we keep the task on the page, saved in local storage.
+$(".bi-save").each(function handleClick(index) {
+  $(".bi-save").click(handleInput());
 
 
-
-// Create a function that allows user input into the form
-
-
-
-// <script>
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-//(function() {
- // 'use strict';
- // window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-   // var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    //var validation = Array.prototype.filter.call(forms, function(form) {
-      //form.addEventListener('submit', function(event) {
-        //if (form.checkValidity() === false) {
-          //event.preventDefault();
-          //event.stopPropagation();
-        //}
-        //form.classList.add('was-validated');
-    //  }, false);
-   // });
- // }, false);
-//})();
-//</script>
+})
 
 // helpful documentation on bootstrap tables : https://examples.bootstrap-table.com/#methods/append.html#view-source
